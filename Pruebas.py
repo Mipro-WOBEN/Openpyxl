8c43d7421ac3b01ed6c91dbe89796dc4c2959c4f
Mipro-WOBEN <safaerazeus9@gmail.com>
2024-11-08 20:04:48 -0500

    actualice

--- Pruebas.py
+++ Pruebas.py
@@ -1,3 +1,15 @@
-import pandas as pd
-tablaPruebas=pd.read_excel("Tablas_dinamicas.xlsx")
-tablaPruebas.head(5)
+import openpyxl as opx
+
+df = opx.load_workbook("Tablas_dinamicas.xlsx")
+dw = df.active
+values = [dw.cell(row=1, column=i).value for i in range(1, dw.max_column)]
+valuesrow = [dw.cell(row=i, column=1).value for i in range(1, dw.max_row)]
+
+my_list = list()
+
+for value in dw.iter_rows(
+    min_row=1, max_row=11, min_col=1, max_col=6, 
+    values_only=True):
+    my_list.append(value)
+for item in my_list:
+    print(item)
